7e831b57a72127eec67db1ea9ec102350e4e3c88
vongoctam95 <tamvnps07298@fpt.edu.vn>
2018-09-13 16:53:21 +0700

    update new files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,49 @@
 // Main
-$(document).ready(function () {
-	// Phần Slider
-	$('.home-slider .owl-carousel').owlCarousel({
-		items: 1,
-		nav: false,
-		dots: true,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-	});
-	// Phần Clients
-	$('.home-clients .owl-carousel').owlCarousel({
-		items: 1,
-		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		responsive: {
-			// breakpoint from 480 up
-			480: {
-				items: 2,
-			},
-			// breakpoint from 768 up
-			768: {
-				items: 4,
-			},
-			// breakpoint from 992 up
-			992: {
-				items: 6,
-			}
-		}
-	});
+$(document).ready(function() {
+    // Phần Slider
+    $('.home-slider .owl-carousel').owlCarousel({
+        items: 1,
+        nav: false,
+        dots: true,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    });
+    // phan top sale
+    $('.home-topsale .owl-carousel').owlCarousel({
+            loop: false,
+            margin: 10,
+            nav: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+        // Phần Clients
+    $('.home-clients .owl-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+        responsive: {
+            // breakpoint from 480 up
+            480: {
+                items: 2,
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 4,
+            },
+            // breakpoint from 992 up
+            992: {
+                items: 6,
+            }
+        }
+    });
 });
